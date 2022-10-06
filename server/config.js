8d71f7e19ebcaee859a6b80ddd10999900ae1f3a
Mauricio <magratea.sa@gmail.com>
2022-10-06 17:07:26 -0500

    update index.js config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,10 @@
+const config = {};
+// export const PORT =  process.env.PORT || 3000;
+const PORT =  process.env.PORT || 3000;
 
-//export const PORT =  process.env.PORT || 3000;
+
+config.PORT = PORT;
+
+module.exports = config;
 
 // "type": "module",
--- server/index.js
+++ server/index.js
@@ -1,68 +1,3 @@
-/*
-//===================================================
-//-- PARA IMPORTAR Y NO USAR REQUIRE
-//   SE AGRAGA "type": "module" AL package.json 
-//      const morgan = require('morgan');
-//      const express = require("express");  
-//===================================================
-//  SE EJECUTA CON ...
-//  npm run dev O CON
-//  nodemon server/index.js
-//
-//  http://localhost:4000/
-//===================================================
-//-- MODULO DE SERVIDOR MEJORADO
-import express from "express";
-//-- MODULO DE TIEMPO DE CARGA (CONSOLA)
-import morgan from "morgan";
-//-- MODULO DE COMUNICACION
-import {Server as SocketServer} from "socket.io";
-//-- MODULO DE PROTOCOLO HTTP
-import http from "http";
-//-- PARA CONECTARSE CON SOCKET.IO DESDE OTROS PUERTOS
-//import cors from "cors";
-import {PORT} from "./config.js";
-//-- PARA CREAR LA VARIABLE __dirname - CON REQUIRE SI EXISTE 
-//   PERO CON IMPORT HAY Q CREARLA
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-//===================================================
-
-const app = express();
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-//console.log(join(__dirname, "../public"));
-//-- SOCKET.IO REQUIERE CREAR UN SERVER HTTP
-const server = http.createServer(app);
-const io = new SocketServer(server);
-//--------------------------------------------
-//app.use(cors()); 
-//app.use(morgan("tiny"));
-//app.use(express.static("public")); 
-app.use(express.static(join(__dirname, "../public")));   
-//--------------------------------------------
-
-
-io.on("connection", newConnection);
-function newConnection(socket) {
-    console.log("new connection: "+socket.id); 
-    
-    socket.on("mouse", mouseMsg);
-    function mouseMsg(data) {
-        socket.broadcast.emit("mouse", data);
-        //console.log(data); 
-    }
-    
-}
-
- 
-//--------------------------------------------
-//app.listen(PORT);
-server.listen(PORT);
-//console.log("Server2 on port", PORT);
-console.log("Socket server is running on "+PORT);  
-//--------------------------------------------
-*/
 
 //===================================================
 //-- PARA IMPORTAR Y NO USAR REQUIRE
@@ -95,7 +30,8 @@ app.use(morgan("tiny"));
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 //-- PUERTO
-const PORT = process.env.PORT || 3000;
+const config = require("./config.js");
+//const PORT = process.env.PORT || 3000;
 //-- DIRECTORIO WEB
 const join = require("path").join;
 app.use(express.static(join(__dirname, "../public")));  
@@ -103,8 +39,8 @@ app.use(express.static(join(__dirname, "../public")));
 //===================================================
 //-- SERVER EN ESCUCHA DE EVENTOS
 //===================================================
-server.listen(PORT, () => {
-    console.log("Socket server is running on "+PORT);
+server.listen(config.PORT, () => {
+    console.log("Socket server is running on "+config.PORT);
 });
 
 //===================================================
@@ -124,32 +60,51 @@ function newConnection(socket) {
 
 
 
-//-- LO QUE FUNCIONA
 /*
-const express = require("express"); 
+//===================================================
+//===================================================
+//-- MODULO DE SERVIDOR MEJORADO
+import express from "express";
+//-- MODULO DE TIEMPO DE CARGA (CONSOLA)
+import morgan from "morgan";
+//-- MODULO DE COMUNICACION
+import {Server as SocketServer} from "socket.io";
+//-- MODULO DE PROTOCOLO HTTP
+import http from "http";
+//-- PARA CONECTARSE CON SOCKET.IO DESDE OTROS PUERTOS
+//import cors from "cors";
+import {PORT} from "./config.js";
+//-- PARA CREAR LA VARIABLE __dirname - CON REQUIRE SI EXISTE 
+//   PERO CON IMPORT HAY Q CREARLA
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+//===================================================
+
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+//--------------------------------------------
+//-- SOCKET.IO REQUIERE CREAR UN SERVER HTTP
+const server = http.createServer(app);
+const io = new SocketServer(server);
+//--------------------------------------------
+//app.use(cors()); 
+app.use(morgan("tiny"));
+app.use(express.static(join(__dirname, "../public")));   
+//--------------------------------------------
 
-//const app = require('express')(); 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
-const join = require("path").join;
-
-
-//app.get('/', (req, res) => {
-//  res.sendFile(join(__dirname, "../public/index.html"));
-//});
-
-
-app.use(express.static(join(__dirname, "../public"))); 
-io.on('connection', (socket) => {
-  socket.on('mouse', msg => {
-    io.emit('mouse', msg);
-  });
-});
-
-//http.listen(port, () => {
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+io.on("connection", newConnection);
+function newConnection(socket) {
+    console.log("new connection: "+socket.id); 
+    
+    socket.on("mouse", mouseMsg);
+    function mouseMsg(data) {
+        socket.broadcast.emit("mouse", data);
+        //console.log(data); 
+    }
+    
+}
+//--------------------------------------------
+server.listen(PORT);
+console.log("Socket server is running on "+PORT);  
+//===================================================
 */
